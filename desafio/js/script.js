c9e939b2aa732db5b040f168383e3aab132bdc62
MARCOS MARTINHO DE BORBA JUNIOR <marcos.mbjunior1@senacsp.edu.br>
2025-05-12 14:49:59 -0300

    atv 1 2 3 e desafio completos

--- desafio/js/script.js
+++ desafio/js/script.js
@@ -4,10 +4,7 @@ while (opcao !== "1" && opcao !== "2") {
     opcao = prompt("Escolha a conversão:\n1 - Celsius para Fahrenheit\n2 - Fahrenheit para Celsius");
 }
 
-let entrada = Number(prompt("Digite a temperatura:"));
-
-let temperatura = entrada;
-
+let temperatura = Number(prompt("Digite a temperatura:"));
 
 if (opcao === "1") {
     let resultado = (temperatura * 1.8) + 32;
